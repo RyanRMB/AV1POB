fe51f19a0a9b858c69baed22cd024c2090206a0d
RyanRMB <142849567+RyanRMB@users.noreply.github.com>
2023-10-19 12:07:52 -0300

    Create carrinho.java

---
+++ lista/carrinho.java
@@ -0,0 +1,15 @@
+package carrinho;
+
+public class carrinho {
+	public int item;
+		public carrinho(int item){
+			this.item = item;
+		}
+		public int getItem() {
+			return item;
+		}
+		public void setItem(int item) {
+			this.item = item;
+		}
+	
+}
