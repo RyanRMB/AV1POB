1af3386c1627943f6cc2238f639676b291c5b597
RyanRMB <142849567+RyanRMB@users.noreply.github.com>
2023-10-19 21:40:44 -0300

    Update principal.java

--- principal.java
+++ principal.java
@@ -5,47 +5,86 @@ import java.util.Scanner;
 public class principal {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		int menu = 3;
-		while(menu!=0) {
-			
-			System.out.println("Digite o número correspondente a opção desejada:\n1 - Adicionar itens ao carrinho.\n2 - Adicionar produtos ao catálogo.\n0 - Encerrar programa.");
+		 listaProduto lista = new listaProduto();
+		 lista.getProduto().add(new listaProduto("Shampoo Loreal", 001, 15));
+		 lista.getProduto().add(new listaProduto("Pasta de Dente Colgate", 002, 5));
+		 lista.getProduto().add(new listaProduto("Cheetos", 003, 6));
+		carrinho carrinho = new carrinho();
+		Scanner sc = new Scanner(System.in);
+		while(menu>0) {
+			System.out.println("Digite o número correspondente a opção desejada:\n1 - Adicionar itens ao carrinho.\n2 - Adicionar produtos ao catálogo.\n3 - Ver a lista de produtos.\n4 - Ver o carrinho.\n0 - Encerrar programa.");
 			menu = sc.nextInt();
-			if(menu == 1) 
+			switch (menu)
 			{
-				comprar();
-			} else if(menu == 2) {
-				adicionarItem();
+				case 1: 
+					comprar(carrinho, lista);
+					break;
+				case 2:
+					adicionarItem();
+					break;
+				case 3:
+					listarProduto(lista);
+					break;
+				case 4:
+					listarCarrinho(carrinho);
+					break;
+				default:
+					System.out.println("Sessão encerrada.");
 			}
 		}
 		sc.close();
 	}
-	
+
+	private static listaCarrinho comprar(carrinho carrinho, listaProduto lista) {
+		int idProduto = 0;
+	    int quant = 0;
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Digite o id do produto\n>> ");
+	    idProduto = sc.nextInt();
+	    System.out.println("Digite quantas unidades do produto você deseja comprar\n>> ");
+	    quant=sc.nextInt();
+	    listaProduto produto = null;
+	    for (listaProduto p : lista.getProduto()) {
+	        if (p.getId() == idProduto) {
+	            produto = p;
+	            break;
+	        }
+	    }
+	    if (produto != null) {
+	        double valor = produto.getValor() * quant;
+	        listaCarrinho compra = new listaCarrinho(idProduto, quant);
+	        carrinho.adicionarCompra(produto, quant, valor);
+	        return compra;
+	    } else {
+	    	System.out.println("Produto não encontrado.");
+	    }
+		return null;
+	}
+
+
 	private static listaProduto adicionarItem () {
 		
 		String nome = null;
 		int id = 0;
 		int valor = 0;
-		
+		System.out.println("lista produto percorrida");
 		listaProduto addLista = new listaProduto(nome, id, valor);
 		return addLista;
 	} 
-
-}
-
-	private static carrinho comprar() 
-	{
-		int idProduto;
-		int quant;
-		char menu = 'S';
-		Scanner sc = new Scanner(System.in);
-		while(menu == 's') 
-		{
-			System.out.println("Digite o id do produto: ");
-			nome = sc.next();
-		}
-			sc.close();
+	
+	private static void listarProduto(listaProduto lista) {
+		System.out.println("Lista de produtos:\n");
+        for (listaProduto p : lista.getProduto())
+        {
+            System.out.printf("Produto: %s\nId: %d\nValor: %d R$\n\n", p.getNome(), p.getId(), p.getValor());
+        }
 		
-		carrinho compra = new compra(idProduto, quant);
-		return compra;
 	}
+	
+	private static listaCarrinho listarCarrinho(carrinho carrinho) 
+	{
+		return null;
+		
+	}
+}
