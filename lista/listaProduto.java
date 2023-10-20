77ea3e75e65b116452c0a7bedd9d29dd77ce0e72
RyanRMB <142849567+RyanRMB@users.noreply.github.com>
2023-10-19 21:40:04 -0300

    Update listaProduto.java

--- lista/listaProduto.java
+++ lista/listaProduto.java
@@ -1,24 +1,32 @@
 package carrinho;
 
+import java.util.ArrayList;
+
 public class listaProduto {
-	private String[] produto = {"Shampoo Loreal","001", "15","Pasta de Dente Colgate", "002","5","Cheetos", "002", "6"};
+	private ArrayList<listaProduto> produto;
 	private String nome;
 	private int id;
 	private int valor;
 	
-	public listaProduto(String[] produto){
+	public listaProduto(){
+		this.produto = new ArrayList<>(); // inicializa a lista de produto
+	}
+
+	public listaProduto(ArrayList<listaProduto> produto){
 		this.produto = produto;
 	}
-
+	
 	public listaProduto(String nome, int id, int valor) {
-		
+		this.nome = nome;
+		this.id = id;
+		this.valor = valor;
 	}
 
-	public String[] getProduto() {
+	public ArrayList<listaProduto> getProduto() {
 		return produto;
 	}
 
-	public void setProduto(String[] produto) {
+	public void setProduto(ArrayList<listaProduto> produto) {
 		this.produto = produto;
 	}
 
@@ -45,4 +53,5 @@ public class listaProduto {
 	public void setValor(int valor) {
 		this.valor = valor;
 	}
+	
 }
